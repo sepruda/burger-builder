8a2d7c40161927040d9029616380da51eab4aa75
Christian Jess Mark <christianjessmark@gmail.com>
2018-09-16 22:10:25 +0200

    Initial commit

---
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import classes from './BuildControl.css';
+
+const buildControl = (props) => (
+    <div className={classes.BuildControl}>
+        <div className={classes.Label}>{props.label}</div>
+        <button 
+            className={classes.less} 
+            onClick={props.removed} disabled={props.disabled}>Less
+        </button>
+        <button 
+            className={classes.more} 
+            onClick={props.added}>More
+        </button>
+    </div>
+);
+
+export default buildControl
---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,35 @@
+import React, { Component} from 'react';
+
+import Aux from '../../hoc/Aux';
+import classes from './Layout.css';
+import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
+
+class Layout extends Component {
+    state = {
+        showSideDrawer: false
+    }
+
+    sideDrawerToggleHandler = () => {
+        this.setState(( prevState ) => {
+            return {showSideDrawer: !prevState.showSideDrawer};
+        });
+    }
+    
+
+    render() {
+        return (
+            <Aux>
+                <Toolbar toggle={this.sideDrawerToggleHandler}/>
+                <SideDrawer 
+                    closed={this.sideDrawerToggleHandler }
+                    open={this.state.showSideDrawer}
+                />
+                <main className={classes.Content}>
+                    {this.props.children}
+                </main>
+            </Aux>
+            );
+    }
+}
+export default Layout;
