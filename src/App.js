7cd481222f0df45c0562a0198d4d0525b32e1661
Christian Jess Mark <christianjessmark@gmail.com>
2018-10-21 22:40:42 +0200

    Add auth views and navigation

--- src/App.js
+++ src/App.js
@@ -5,19 +5,21 @@ import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
 import Checkout from "./containers/Checkout/Checkout";
 import { Route, Switch } from "react-router-dom";
 import Orders from "./containers/Orders/Orders";
+import Auth from "./containers/Auth/Auth";
 
 class App extends Component {
-    render() {
-        return (
-            <Layout>
-                <Switch>
-                    <Route path="/checkout" component={Checkout} />
-                    <Route path="/orders" component={Orders} />
-                    <Route exact path="/" component={BurgerBuilder} />
-                </Switch>
-            </Layout>
-        );
-    }
+  render() {
+    return (
+      <Layout>
+        <Switch>
+          <Route path="/checkout" component={Checkout} />
+          <Route path="/orders" component={Orders} />
+          <Route path="/auth" component={Auth} />
+          <Route exact path="/" component={BurgerBuilder} />
+        </Switch>
+      </Layout>
+    );
+  }
 }
 
 export default App;
--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -4,12 +4,13 @@ import classes from "./NavigationItems.css";
 import NavigationItem from "./NavigationItem/NavigationItem";
 
 const navigationItems = props => (
-    <ul className={classes.NavigationItems}>
-        <NavigationItem exact link="/">
-            Burger Builder
+  <ul className={classes.NavigationItems}>
+    <NavigationItem exact link="/">
+      Burger Builder
         </NavigationItem>
-        <NavigationItem link="/orders">Orders</NavigationItem>
-    </ul>
+    <NavigationItem link="/orders">Orders</NavigationItem>
+    <NavigationItem link="/auth">Sign in</NavigationItem>
+  </ul>
 );
 
 export default navigationItems;
